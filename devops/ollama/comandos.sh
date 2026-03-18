848865996a6799976dbf1aa9cef79f13b97eb3ab
morgado <morgadodesarrollador@gmail.com>
2026-03-18 13:46:24 +0100

    IA

--- devops/ollama/comandos.sh
+++ devops/ollama/comandos.sh
@@ -10,3 +10,7 @@ kubectl get all -n ollama
 kubectl get pods -n ollama -w
 kubectl exec -it deploy/ollama -n ollama -- ollama pull llama3:8b
 kubectl exec -it deploy/ollama -n ollama -- ollama pull deepseek-coder
+
+curl -N http://ollama.jamorgado.es/api/generate   -d '{"model":"llama3:8b","prompt":"hola","stream":true}'
+
+kubectl exec -it $(kubectl get pod -l app=ollama -n ollama -o jsonpath='{.items[0].metadata.name}') -n ollama -- cat /proc/meminfo | head -5
